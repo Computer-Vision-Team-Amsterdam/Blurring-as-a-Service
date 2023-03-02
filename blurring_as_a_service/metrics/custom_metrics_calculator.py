b145ed6c5710bbc6449a95e0e147cce3c6751ed6
Diana <epureanudiana13@gmail.com>
2023-03-02 15:20:08 +0100

    refactoring

--- blurring_as_a_service/metrics/custom_metrics_calculator.py
+++ blurring_as_a_service/metrics/custom_metrics_calculator.py
@@ -1,10 +1,28 @@
 import itertools
 import json
+from enum import Enum
 from os import listdir
 from os.path import isfile, join
 from typing import List
 
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+from blurring_as_a_service.metrics.metrics_utils import generate_binary_mask
+from blurring_as_a_service.metrics.total_blurred_area import TotalBlurredArea
+from blurring_as_a_service.metrics.yolo_labels_dataset import YoloLabelsDataset
 from blurring_as_a_service.utils.bias_category_mapper import BiasCategoryMapper
+
+
+class Size(Enum):
+    small = [0, 5000]
+    medium = [5000, 10000]
+    large = [10000, 1000000]
+
+
+class Class(Enum):
+    person = 0
+    licence_plate = 1
 
 
 class CustomMetricsCalculator:
@@ -30,10 +48,12 @@ class CustomMetricsCalculator:
         coco_file_with_categories
             File containing the categories.
         """
+
         self._get_and_prepare_categories(coco_file_with_categories)
         self._calculate_true_positives_and_false_negatives_per_category(
             tagged_validation_folder
         )
+        self._get_gt_boxes_and_predicted_boxes_per_category(tagged_validation_folder)
 
     def calculate_and_store_metrics(self, markdown_output_path):
         """
@@ -49,16 +69,27 @@ class CustomMetricsCalculator:
 
         with open(markdown_output_path, "w") as f:
             f.write(
-                "| Category | Value | True Positives | False Negatives | False Negative Rate |\n"
+                "| Category | Value | True Positives | TP Small | TP Medium | TP Large "
+                "| False Negatives | FN Small | FN Medium | FN Large | False Negative Rate | False Negative Rate Small "
+                "|False Negative Rate Medium | False Negative Rate Large|\n"
             )
             f.write(
-                "| -------- | ----- | -------------- | --------------- | ------------------ |\n"
+                "| -------- | ----- | ----- | ----- | ----- | ----- | ----- | ----- | ----- | ----- | ----- |"
+                "-------------- | --------------- | ------------------ |\n"
             )
             for item in result:
                 f.write(
                     f'| {item["category"]} | {item["value"]} | {item["true_positives"]} '
-                    f'| {item["false_negatives"]} | {item["false_negative_rate"]} |\n'
+                    f'| {item["true_positives_small"]} | {item["true_positives_medium"]} | {item["true_positives_large"]} '
+                    f'| {item["false_negatives"]} | {item["false_negatives_small"]} | {item["false_negatives_medium"]} '
+                    f'| {item["false_negatives_large"]} | {item["false_negative_rate"]}'
+                    f'| {item["false_negative_rate_small"]} | {item["false_negative_rate_medium"]}'
+                    f'| {item["false_negative_rate_large"]}\n'
                 )
+            f.write(
+                f"Thresholds used for these calculations: Small=`{Size.small.value}`, Medium=`{Size.medium.value}` "
+                f"and Large=`{Size.large.value}`."
+            )
 
     def calculate_false_negative_rate_for_all_categories(self) -> List[dict]:
         """
@@ -72,7 +103,15 @@ class CustomMetricsCalculator:
         A list where each element is a category and its metrics, included the false negative rate.
         """
         result = []
-        for category in ["grouped_category", "sex", "age", "skin_color"]:
+        for category in [
+            "grouped_category",
+            "sex",
+            "age",
+            "skin_color",
+            "licence_plate_origin",
+            "licence_plate_color",
+        ]:
+            print(category)
             result.append(self.calculate_false_negative_rate_for_a_category(category))
         return list(itertools.chain(*result))
 
@@ -97,7 +136,13 @@ class CustomMetricsCalculator:
         all_categories = {
             available_category: {
                 "true_positives": 0,
+                "true_positives_small": 0,
+                "true_positives_medium": 0,
+                "true_positives_large": 0,
                 "false_negatives": 0,
+                "false_negatives_small": 0,
+                "false_negatives_medium": 0,
+                "false_negatives_large": 0,
             }
             for available_category in available_categories
         }
@@ -105,24 +150,70 @@ class CustomMetricsCalculator:
             category = all_categories[value[category_name]]
             category["true_positives"] += value["true_positives"]
             category["false_negatives"] += value["false_negatives"]
+            category["true_positives_small"] += value["true_positives_small"]
+            category["true_positives_medium"] += value["true_positives_medium"]
+            category["true_positives_large"] += value["true_positives_large"]
+            category["false_negatives_small"] += value["false_negatives_small"]
+            category["false_negatives_medium"] += value["false_negatives_medium"]
+            category["false_negatives_large"] += value["false_negatives_large"]
 
         result = []
         for name, values in all_categories.items():
             true_positives = values["true_positives"]
             false_negatives = values["false_negatives"]
-            if false_negatives + true_positives > 0:
-                false_negative_rate = self._calculate_false_negative_rate(
-                    false_negatives, true_positives
+            true_positives_small = values["true_positives_small"]
+            true_positives_medium = values["true_positives_medium"]
+            true_positives_large = values["true_positives_large"]
+            false_negatives_small = values["false_negatives_small"]
+            false_negatives_medium = values["false_negatives_medium"]
+            false_negatives_large = values["false_negatives_large"]
+
+            false_negative_rate = (
+                self._calculate_false_negative_rate(false_negatives, true_positives)
+                if false_negatives + true_positives > 0
+                else None
+            )
+
+            false_negative_rate_small = (
+                self._calculate_false_negative_rate(
+                    false_negatives_small, true_positives_small
                 )
-            else:
-                false_negative_rate = None
+                if false_negatives_small + true_positives_small > 0
+                else None
+            )
+
+            false_negative_rate_medium = (
+                self._calculate_false_negative_rate(
+                    false_negatives_medium, true_positives_medium
+                )
+                if false_negatives_medium + true_positives_medium > 0
+                else None
+            )
+
+            false_negative_rate_large = (
+                self._calculate_false_negative_rate(
+                    false_negatives_large, true_positives_large
+                )
+                if false_negatives_large + true_positives_large > 0
+                else None
+            )
+
             result.append(
                 {
                     "category": category_name,
                     "value": name,
                     "true_positives": true_positives,
+                    "true_positives_small": true_positives_small,
+                    "true_positives_medium": true_positives_medium,
+                    "true_positives_large": true_positives_large,
                     "false_negatives": false_negatives,
+                    "false_negatives_small": false_negatives_small,
+                    "false_negatives_medium": false_negatives_medium,
+                    "false_negatives_large": false_negatives_large,
                     "false_negative_rate": false_negative_rate,
+                    "false_negative_rate_small": false_negative_rate_small,
+                    "false_negative_rate_medium": false_negative_rate_medium,
+                    "false_negative_rate_large": false_negative_rate_large,
                 }
             )
         return result
@@ -142,7 +233,7 @@ class CustomMetricsCalculator:
         Exception in case the len of GT_boxes, GT_labels and TP_labels are not matching.
 
         """
-        for tagged_validation_file in self._get_all_filenames_in_dir(
+        for tagged_validation_file in self.get_all_filenames_in_dir(
             tagged_validation_folder
         ):
             with open(f"{tagged_validation_folder}/{tagged_validation_file}") as f:
@@ -156,13 +247,73 @@ class CustomMetricsCalculator:
                         f"{tagged_validation_file} not well formed, "
                         f"the len of GT_boxes, GT_labels and TP_labels is not matching."
                     )
-                for i, gt_label in enumerate(tagged_validation_content["GT_labels"]):
+
+                for i, (gt_box, gt_label) in enumerate(
+                    zip(
+                        tagged_validation_content["GT_boxes"],
+                        tagged_validation_content["GT_labels"],
+                    )
+                ):
                     category_stats = self._statistics_per_category[gt_label]
-                    category_stats[
-                        "true_positives"
-                        if tagged_validation_content["TP_labels"][i]
-                        else "false_negatives"
-                    ] += 1
+                    bbox_area = gt_box[2] * gt_box[3] * 32000000
+                    category_stats["area"] = bbox_area
+                    if tagged_validation_content["TP_labels"][i]:
+                        category_stats["true_positives"] += 1
+                        if Size.small.value[0] <= bbox_area < Size.small.value[1]:
+                            category_stats["true_positives_small"] += 1
+                        if Size.medium.value[0] <= bbox_area < Size.medium.value[1]:
+                            category_stats["true_positives_medium"] += 1
+                        if Size.large.value[0] <= bbox_area < Size.large.value[1]:
+                            category_stats["true_positives_large"] += 1
+                    else:
+                        category_stats["false_negatives"] += 1
+                        if Size.small.value[0] <= bbox_area < Size.small.value[1]:
+                            category_stats["false_negatives_small"] += 1
+                        if Size.medium.value[0] <= bbox_area < Size.medium.value[1]:
+                            category_stats["false_negatives_medium"] += 1
+                        if Size.large.value[0] <= bbox_area < Size.large.value[1]:
+                            category_stats["false_negatives_large"] += 1
+
+    def _get_gt_boxes_and_predicted_boxes_per_category(
+        self, tagged_validation_folder: str
+    ):
+        """
+        For each category of each file in tagged_validation_folder it gathers the ground truth boxes and all
+         predicted boxes. This will be later used in computing the total blurred area per category.
+        Args:
+            tagged_validation_folder: Path to the folder containing all the tagged validation values.
+
+        Returns:
+
+        """
+        for tagged_validation_file in self.get_all_filenames_in_dir(
+            tagged_validation_folder
+        ):
+            with open(f"{tagged_validation_folder}/{tagged_validation_file}") as f:
+                tagged_validation_content = json.load(f)
+                if any(
+                    len(tagged_validation_content[key])
+                    != len(tagged_validation_content["GT_labels"])
+                    for key in ["GT_boxes", "TP_labels"]
+                ):
+                    raise Exception(
+                        f"{tagged_validation_file} not well formed, "
+                        f"the len of GT_boxes, GT_labels and TP_labels is not matching."
+                    )
+
+                for i, (gt_box, gt_label) in enumerate(
+                    zip(
+                        tagged_validation_content["GT_boxes"],
+                        tagged_validation_content["GT_labels"],
+                    )
+                ):
+                    category_stats = self._statistics_per_category[gt_label]
+                    category_stats["gt_boxes"].append(
+                        [tagged_validation_content["GT_boxes"][i]]
+                    )
+                    category_stats["pred_boxes"].append(
+                        tagged_validation_content["Pred_boxes"]
+                    )
 
     def _get_and_prepare_categories(self, coco_file_with_categories):
         """
@@ -183,28 +334,72 @@ class CustomMetricsCalculator:
             categories_file_content["categories"]
         )
         all_grouped_categories = self._bias_category_mapper.get_all_grouped_categories()
-        self._statistics_per_category = {
-            d["id"]: {
-                "id": d["id"],
-                "grouped_category": d["grouped_category"],
-                "sex": sex,
-                "age": age,
-                "skin_color": skin_color,
-                "true_positives": 0,
-                "false_negatives": 0,
-            }
-            for d in all_grouped_categories
-            for sex, age, skin_color in [
+
+        self._statistics_per_category = {}
+        for d in all_grouped_categories:
+            for attr_1, attr_2, attr_3 in [
                 d["name"].split("/") + [""] * (3 - len(d["name"].split("/")))
-            ]
-        }
+            ]:
+                if attr_1 == "licence_plate":
+                    self._statistics_per_category[d["id"]] = {
+                        "id": d["id"],
+                        "grouped_category": d["grouped_category"],
+                        "sex": "",
+                        "age": "",
+                        "skin_color": "",
+                        "licence_plate_origin": attr_2,
+                        "licence_plate_color": attr_3,
+                        "true_positives": 0,
+                        "true_positives_small": 0,
+                        "true_positives_medium": 0,
+                        "true_positives_large": 0,
+                        "false_negatives": 0,
+                        "false_negatives_small": 0,
+                        "false_negatives_medium": 0,
+                        "false_negatives_large": 0,
+                        "area": 0,
+                        "gt_boxes": [],
+                        "pred_boxes": [],
+                    }
+                else:
+                    self._statistics_per_category[d["id"]] = {
+                        "id": d["id"],
+                        "grouped_category": d["grouped_category"],
+                        "sex": attr_1,
+                        "age": attr_2,
+                        "skin_color": attr_3,
+                        "licence_plate_origin": "",
+                        "licence_plate_color": "",
+                        "true_positives": 0,
+                        "true_positives_small": 0,
+                        "true_positives_medium": 0,
+                        "true_positives_large": 0,
+                        "false_negatives": 0,
+                        "false_negatives_small": 0,
+                        "false_negatives_medium": 0,
+                        "false_negatives_large": 0,
+                        "area": 0,
+                        "gt_boxes": [],
+                        "pred_boxes": [],
+                    }
+
+    def get_areas(self):
+        return [value["area"] for key, value in self._statistics_per_category.items()]
+
+    def _plot_area_distribution(self, path: str, plot_name: str):
+        plt.hist(self.get_areas(), bins=50, range=[0, 25000], color="blue", alpha=0.5)
+        plt.xlabel("Values")
+        plt.ylabel("Frequency")
+        plt.title(f"Histogram of bbox area of {plot_name}")
+        plt.savefig(f"{path}/{plot_name}.jpg")
+        plt.show()
 
     @staticmethod
-    def _get_all_filenames_in_dir(directory_path: str) -> List[str]:
+    def get_all_filenames_in_dir(directory_path: str) -> List[str]:
         return [f for f in listdir(directory_path) if isfile(join(directory_path, f))]
 
     @staticmethod
-    def _calculate_false_negative_rate(false_negatives: int, true_positives: int):
+    def _calculate_false_negative_rate(false_negatives, true_positives):
         """
         Calculates the False Negative Rate metric.
         Finding no disparities in this metric means that we would have the same chance of not blurring someone
@@ -221,4 +416,85 @@ class CustomMetricsCalculator:
         -------
         False negative rate.
         """
-        return false_negatives / (false_negatives + true_positives)
+        return round(false_negatives / (false_negatives + true_positives), 3)
+
+
+def get_total_blurred_area_statistics(true_labels, predicted_lables):
+    total_blurred_area = TotalBlurredArea()
+
+    for image_id in tqdm(true_labels.keys(), total=len(true_labels)):
+        tba_true_mask = generate_binary_mask(true_labels[image_id][:, 1:5])
+        tba_pred_mask = generate_binary_mask(predicted_lables[image_id][:, 1:5])
+
+        total_blurred_area.update_statistics_based_on_masks(
+            true_mask=tba_true_mask, predicted_mask=tba_pred_mask
+        )
+
+    results = total_blurred_area.get_statistics()
+
+    return results
+
+
+def collect_tba_results_per_class_and_size(true_path, pred_path):
+    predicted_dataset = YoloLabelsDataset(folder_path=pred_path)
+    results = {}
+
+    # ====== PERSONS ====== #
+
+    for size in Size:
+        true_persons_size = (
+            YoloLabelsDataset(folder_path=true_path)
+            .filter_by_class(class_to_keep=Class.person.value)
+            .filter_by_size(size_to_keep=size.value)
+            .get_filtered_labels()
+        )
+        results[f"persons_{size.name}"] = get_total_blurred_area_statistics(
+            true_persons_size, predicted_dataset.get_labels()
+        )
+
+    # ====== LICENCE PLATE ====== #
+
+    for size in Size:
+        true_licences_size = (
+            YoloLabelsDataset(folder_path=true_path)
+            .filter_by_class(class_to_keep=Class.licence_plate.value)
+            .filter_by_size(size_to_keep=size.value)
+            .get_filtered_labels()
+        )
+        results[f"licences_{size.name}"] = get_total_blurred_area_statistics(
+            true_licences_size, predicted_dataset.get_labels()
+        )
+
+    return results
+
+
+def store_tba_result(results, markdown_output_path="tba_scores.mda"):
+    with open(markdown_output_path, "w") as f:
+        f.write(
+            " Person Small | Person Medium | Person Large |"
+            " License Plate Small |  License Plate Medium  | License Plate Large |\n"
+        )
+        f.write("|----- | ----- |  ----- | ----- | ----- | ----- |\n")
+        f.write(
+            f'| {results["persons_small"]["recall"]} | {results["persons_medium"]["recall"]} '
+            f'| {results["persons_large"]["recall"]}| {results["licences_small"]["recall"]} '
+            f'| {results["licences_medium"]["recall"]} | {results["licences_large"]["recall"]}|\n'
+        )
+
+        f.write(
+            f"Thresholds used for these calculations: Small=`{Size.small.value}`, Medium=`{Size.medium.value}` "
+            f"and Large=`{Size.large.value}`."
+        )
+        f.write(
+            f"Thresholds used for these calculations: Small=`{Size.small.value}`, Medium=`{Size.medium.value}` "
+            f"and Large=`{Size.large.value}`."
+        )
+
+
+def collect_and_store_tba_results_per_class_and_size(
+    ground_truth_path, predictions_path, markdown_output_path
+):
+    results = collect_tba_results_per_class_and_size(
+        ground_truth_path, predictions_path
+    )
+    store_tba_result(results, markdown_output_path)
--- blurring_as_a_service/metrics/total_blurred_area.py
+++ blurring_as_a_service/metrics/total_blurred_area.py
@@ -3,34 +3,55 @@ import numpy as np
 
 class TotalBlurredArea:
     def __init__(self):
+        self.f1_score = None
+        self.recall = None
+        self.precision = None
         self.tp = 0
         self.fp = 0
         self.tn = 0
         self.fn = 0
 
-    def add_mask(self, true_mask, pred_mask):
+    def update_statistics_based_on_masks(self, true_mask, predicted_mask):
         """
+        Computes statistics for a given pair of binary masks.
+        Args:
+            true_mask: numpy array of shape (height, width)
+            predicted_mask: numpy array of shape (height, width)
+
+        Returns:
+
+        """ """
         Computes true positives and false positives for a given pair of binary masks.
         :param true_mask: numpy array of shape (height, width)
         :param pred_mask: numpy array of shape (height, width)
         """
-        tp = np.sum((true_mask == 1) & (pred_mask == 1))
-        fp = np.sum((true_mask == 0) & (pred_mask == 1))
-        tn = np.sum((true_mask == 0) & (pred_mask == 0))
-        fn = np.sum((true_mask == 1) & (pred_mask == 0))
+        tp = np.sum((true_mask == 1) & (predicted_mask == 1))
+        fp = np.sum((true_mask == 0) & (predicted_mask == 1))
+        tn = np.sum((true_mask == 0) & (predicted_mask == 0))
+        fn = np.sum((true_mask == 1) & (predicted_mask == 0))
+
         self.tp += tp
         self.fp += fp
         self.tn += tn
         self.fn += fn
 
-    def summary(self, message: str = ""):
-        precision = self.tp / (self.tp + self.fp)
-        recall = self.tp / (self.tp + self.fn)
+    def get_statistics(self):
+        self.precision = (
+            round(self.tp / (self.tp + self.fp), 3) if self.tp + self.fp > 0 else None
+        )
+        self.recall = (
+            round(self.tp / (self.tp + self.fn), 3) if self.tp + self.fn > 0 else None
+        )
+        self.f1_score = round(
+            2 * self.precision * self.recall / (self.precision + self.recall), 3
+        )
 
-        f1 = 2 * precision * recall / (precision + recall)
-
-        print(f"{message}")
-        print(f"TP: {self.tp:,}\nFP: {self.fp:,}\nTN: {self.tn:,}\nFN: {self.fn:,}\n")
-        print(f"Precision: {precision:.3f}.")
-        print(f"Recall: {recall:.3f}.")
-        print(f"F1 score: {f1:.3f}.")
+        return {
+            "true_positives": self.tp,
+            "false_positives": self.fp,
+            "true_negatives": self.tn,
+            "false_negatives:": self.fn,
+            "precision": self.precision,
+            "recall": self.recall,
+            "f1_score": self.f1_score,
+        }
