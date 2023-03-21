ce571e3c287c17246b20d3841dadb5900f598bfe
Diana <epureanudiana13@gmail.com>
2023-03-21 11:41:29 +0100

    test new setup

--- blurring_as_a_service/performace_evaluation_pipeline/components/evaluate_with_cvt_metrics.py
+++ blurring_as_a_service/performace_evaluation_pipeline/components/evaluate_with_cvt_metrics.py
@@ -1,15 +1,15 @@
 import os
 import sys
 
-from mldesigner import Input, command_component
-
-from blurring_as_a_service.metrics.custom_metrics_calculator import (
-    CustomMetricsCalculator,
-    collect_and_store_tba_results_per_class_and_size,
-)
+from mldesigner import Input, command_component  # noqa: E402
 
 sys.path.append("../../..")
 
+
+from blurring_as_a_service.metrics.custom_metrics_calculator import (  # noqa: E402
+    CustomMetricsCalculator,
+    collect_and_store_tba_results_per_class_and_size,
+)
 from blurring_as_a_service.settings.settings import (  # noqa: E402
     BlurringAsAServiceSettings,
 )
@@ -33,9 +33,10 @@ def evaluate_with_cvt_metrics(
     mounted_dataset: Input(type="uri_folder"),  # type: ignore # noqa: F821
     yolo_output_folder: Input(type="uri_folder"),  # type: ignore # noqa: F821
     annotations_for_custom_metrics: Input(type="uri_file"),  # type: ignore # noqa: F821
+    experiment_name: str,  # type: ignore # noqa: F821
 ):
     true_path = f"{mounted_dataset}/labels/val"
-    pred_path = f"{yolo_output_folder}/exp/labels"
+    pred_path = f"{yolo_output_folder}/{experiment_name}/labels"
 
     # ======== Total Blurred Area metric ========= #
     collect_and_store_tba_results_per_class_and_size(
@@ -43,6 +44,7 @@ def evaluate_with_cvt_metrics(
     )
 
     # ======== False Negative Rate metric ========= #
+
     metrics_calculator = CustomMetricsCalculator(
         true_path, annotations_for_custom_metrics
     )
