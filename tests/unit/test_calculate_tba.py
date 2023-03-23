7ab6ec93ed11276111c8181292aebffbb8b63f88
Diana <epureanudiana13@gmail.com>
2023-03-23 11:52:42 +0100

    last test and updated gitignore

---
+++ tests/unit/test_calculate_tba.py
@@ -0,0 +1,28 @@
+from blurring_as_a_service.metrics.custom_metrics_calculator import (
+    collect_and_store_tba_results_per_class_and_size,
+)
+
+
+def expected_output():
+    """
+    Check if the markdown file has been written correctly.
+
+    Returns
+    -------
+    list
+        Expected structure for markdown.
+    """
+    return (
+        "| Person Small | Person Medium | Person Large "
+        "| License Plate Small | License Plate Medium | License Plate Large | \n"
+    )
+
+
+def test_custom_metrics_calculator():
+    true_path = "../../local_test_data/sample-tagged/labels/val"
+    pred_path = "../../yolov5/runs/val/exp23/labels"
+    markdown_output_path = "tba_results.md"
+
+    collect_and_store_tba_results_per_class_and_size(
+        true_path, pred_path, markdown_output_path=markdown_output_path
+    )
