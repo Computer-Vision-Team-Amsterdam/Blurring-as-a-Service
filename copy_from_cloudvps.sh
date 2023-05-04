7b77dab07282b2975680cd33962fb64ff25c5aa2
Diana <epureanudiana13@gmail.com>
2023-05-04 15:57:34 +0200

    script to copy from clouvps to storage account

---
+++ copy_from_cloudvps.sh
@@ -0,0 +1,44 @@
+# Variables
+#relativePath="2016/03/17/"
+relativePath="2016/03/21/"
+storageAccountUrl="https://cvodataweupgwapeg4pyiw5e.blob.core.windows.net/panorama"
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --key)
+            key="$2"
+            shift
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+# Check if the key is not empty
+if [ -z "$key" ]; then
+    echo "Usage: bash copy_from_cloudvps.sh --key cloudvps_password"
+    exit 1
+fi
+
+# Install rclone
+curl https://rclone.org/install.sh | sudo bash
+
+# Add connection to CloudVPS
+rclone config create raw_objectstore_rclone swift auth https://identity.stack.cloudvps.com/v2.0 auth_version 2 tenant 3206eec333a04cc980799f75a593505a user CVT_read_pano key "$key"
+
+echo "Counting files in $relativePath..."
+countOutput=$(rclone size raw_objectstore_rclone:intermediate/"$relativePath")
+echo "rclone size output: $countOutput"
+
+# Copy from CloudVps to Azure Compute to the current working directory. 
+# It creates the "intermediate" in the current working directory.
+rclone copy raw_objectstore_rclone:intermediate/"$relativePath" intermediate/"$relativePath"
+
+# Copy from Azure Compute to Storage Account
+azcopy copy intermediate/ "$storageAccountUrl" --recursive
+
+# Useful command in case the azcopy does not finish successfully:azcopy jobs show <job-id> --with-status=Failed
