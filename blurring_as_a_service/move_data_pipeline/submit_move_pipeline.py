6aa288974732ccad4065c783e7efe97a1df9bc20
Chris Eijgenstein <chrise96@hotmail.com>
2023-05-26 15:04:44 +0200

    Add files via upload

--- blurring_as_a_service/move_data_pipeline/submit_move_pipeline.py
+++ blurring_as_a_service/move_data_pipeline/submit_move_pipeline.py
@@ -15,12 +15,11 @@ from blurring_as_a_service.utils.aml_interface import AMLInterface  # noqa: E402
 @pipeline()
 def move_files_pipeline():
     customer_list = move_data_settings["customers"]
+    azureml_input = move_data_settings["input_container_root"]
+    azureml_output = move_data_settings["output_container_root"]
 
     for customer in customer_list:
         move_data = move_files()
-
-        azureml_input = move_data_settings["input_container_root"]
-        azureml_output = move_data_settings["output_container_root"]
 
         azureml_input_formatted = azureml_input.format(
             subscription=subscription_id,
