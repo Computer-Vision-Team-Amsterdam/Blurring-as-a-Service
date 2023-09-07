3e4c44fb8ce6d57be35be7c6fc17690cc70e835b
Diana <epureanudiana13@gmail.com>
2023-09-07 16:18:15 +0200

    removed comment

--- blurring_as_a_service/utils/logging_handler.py
+++ blurring_as_a_service/utils/logging_handler.py
@@ -2,22 +2,6 @@ import logging
 
 from opencensus.ext.azure.log_exporter import AzureLogHandler
 
-# class CustomLogger:
-#     def __init__(self, cfg):
-#         self._logger = None  # Initialize the logger attribute to None
-#         self.cfg = cfg
-#         self.setup_azure_logging_from_config()
-#
-#     def setup_azure_logging(self):
-#         self._logger = logging.getLogger('opencensus')
-#         self._logger.setLevel(logging.WARNING)
-#         # logger.addHandler(logging.StreamHandler())
-#         instrumentation_key = "InstrumentationKey=179725b3-3e51-4191-9070-d7449d34420b;IngestionEndpoint=https" \
-#                               "://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope" \
-#                               ".livediagnostics.monitor.azure.com/ "
-#         self._logger.handlers.clear()
-#         self._logger.addHandler(AzureLogHandler(connection_string=instrumentation_key))
-
 
 def setup_azure_logging_from_config(cfg):
     logging.basicConfig(**cfg["basic_config"])
