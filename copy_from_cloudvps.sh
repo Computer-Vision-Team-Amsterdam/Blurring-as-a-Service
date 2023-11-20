8f3996e703002ff3748387b80f9a708b7cd233f8
chrise96 <chrise96@hotmail.com>
2023-11-20 09:00:57 +0100

    Update copy_from_cloudvps.sh

--- copy_from_cloudvps.sh
+++ copy_from_cloudvps.sh
@@ -1,7 +1,26 @@
+#!/bin/bash
+
 # Variables
-#relativePath="2016/03/17/"
-relativePath="2016/03/21/"
-storageAccountUrl="https://cvodataweupgwapeg4pyiw5e.blob.core.windows.net/panorama"
+relativePath="2023/05/"
+
+# De naam van het bestand
+filename="todo_$(echo "$relativePath" | tr -d '/')"
+
+# Combining relativePath and filename
+file_path="${filename}.txt"
+
+# Krijg de huidige datum en tijd
+current_datetime=$(date)
+
+# Schrijf de huidige datum en tijd naar het bestand
+echo "$current_datetime" > "$file_path"
+
+# Optioneel: bevestiging dat het bestand is aangemaakt
+if [ -e "$file_path" ]; then
+    echo "Bestand $file_path is aangemaakt met de huidige datum en tijd: '$current_datetime'."
+else
+    echo "Er is een probleem opgetreden bij het maken van $file_path."
+fi
 
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
@@ -34,14 +53,37 @@ echo "Counting files in $relativePath..."
 countOutput=$(rclone size raw_objectstore_rclone:intermediate/"$relativePath")
 echo "rclone size output: $countOutput"
 
-# Copy from CloudVps to Azure Compute to the current working directory. 
+# Copy from CloudVps to Azure Compute to the current working directory.
+# It creates the "intermediate" in the current working directory.
+rclone copy raw_objectstore_rclone:intermediate/"$relativePath" intermediate/"$relativePath"
+
+# De naam van het bestand
+filename="done_$(echo "$relativePath" | tr -d '/')"
+
+# Combining relativePath and filename
+file_path="${filename}.txt"
+
+# Krijg de huidige datum en tijd
+current_datetime=$(date)
+
+# Schrijf de huidige datum en tijd naar het bestand
+echo "$current_datetime" > "$file_path"
+
+# Optioneel: bevestiging dat het bestand is aangemaakt
+if [ -e "$file_path" ]; then
+    echo "Bestand $file_path is aangemaakt met de huidige datum en tijd: '$current_datetime'."
+else
+    echo "Er is een probleem opgetreden bij het maken van $file_path."
+fi
+
+# Copy from CloudVps to Azure Compute to the current working directory.
 # It creates the "intermediate" in the current working directory.
 # NOTE we are not able to do rclone config create for the blob container, so we use AZ copy later
-rclone copy raw_objectstore_rclone:intermediate/"$relativePath" intermediate/"$relativePath"
+# rclone copy raw_objectstore_rclone:intermediate/"$relativePath" intermediate/"$relativePath"
 
 # Copy from Azure Compute to Storage Account
 # NOTE make sure to run "azcopy login" before this line
-echo "!!!!NOTE make sure to run "azcopy login" before this line!!!"
-azcopy copy intermediate/ "$storageAccountUrl" --recursive
+# echo "!!!!NOTE make sure to run "azcopy login" before this line!!!"
+# azcopy copy intermediate/ "$storageAccountUrl" --recursive
 
 # Useful command in case the azcopy does not finish successfully:azcopy jobs show <job-id> --with-status=Failed
