83d40b9153090b1de64b33b329c9b933b97c6eba
chrise96 <chrise96@hotmail.com>
2023-11-28 21:32:38 +0100

    speed

--- blurring_as_a_service/pre_inference_pipeline/components/move_files.py
+++ blurring_as_a_service/pre_inference_pipeline/components/move_files.py
@@ -1,20 +1,13 @@
-import logging
 import os
-import shutil
+import subprocess
 import sys
+from pathlib import Path
 
-from azure.ai.ml.constants import AssetTypes
-from mldesigner import Output, command_component
+from mldesigner import command_component
 
 sys.path.append("../../..")
-from blurring_as_a_service.pre_inference_pipeline.source.image_paths import (  # noqa: E402
-    get_image_paths,
-)
 from blurring_as_a_service.settings.settings import (  # noqa: E402
     BlurringAsAServiceSettings,
-)
-from blurring_as_a_service.settings.settings_helper import (  # noqa: E402
-    setup_azure_logging,
 )
 
 config_path = os.path.abspath(
@@ -23,16 +16,28 @@ config_path = os.path.abspath(
 settings = BlurringAsAServiceSettings.set_from_yaml(config_path)
 aml_experiment_settings = settings["aml_experiment_details"]
 
-# DO NOT import relative paths before setting up the logger.
-# Exception, of course, is settings to set up the logger.
-log_settings = BlurringAsAServiceSettings.set_from_yaml(config_path)["logging"]
-setup_azure_logging(log_settings, __name__)
-
 # Construct the path to the yolov5 package
 yolov5_path = os.path.abspath(
     os.path.join(os.path.dirname(__file__), "..", "..", "..", "yolov5")
 )
 
+
+def run_cmd(cmd, cwd: Path = "./") -> int:
+    """Run the command and returns the result."""
+    result = subprocess.run(
+        cmd,
+        cwd=cwd,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        encoding=sys.stdout.encoding,
+        errors="ignore",
+    )
+    if result.returncode != 0:
+        print(f"Failed with error {result.stdout}.")
+    else:
+        print(f"Successfully executed! Output: \n{result.stdout}")
+    return result.returncode
 
 @command_component(
     name="move_files",
@@ -41,11 +46,7 @@ yolov5_path = os.path.abspath(
     code="../../../",
     is_deterministic=False,
 )
-def move_files(
-    execution_time: str,
-    input_container: Output(type=AssetTypes.URI_FOLDER),  # type: ignore # noqa: F821
-    output_container: Output(type=AssetTypes.URI_FOLDER),  # type: ignore # noqa: F821
-):
+def move_files(execution_time: str):
     """
     Pipeline step to detect the areas to blur.
 
@@ -53,49 +54,16 @@ def move_files(
     ----------
     execution_time:
         Datetime containing when the job was executed. Used to name the folder.
-    input_container:
-        Path of the mounted root folder containing the images.
-    output_container:
-        Where to store the results in a Blob Container.
 
     """
-    logger = logging.getLogger("move_files")
-    # List all files in the mounted folder and their relative paths
-    image_paths = get_image_paths(input_container)
+    src_uri = f"https://{TODO}.blob.core.windows.net/{TODO}-input/*"
+    dest_uri = f"https://{TODO}.blob.core.windows.net/{TODO}-input-structured/{execution_time}/"
 
-    if len(image_paths) == 0:
-        logger.info("No files in the input zone. Aborting...")
+    # TODO what if we need to do azcopy login?
+    download_cmd = f"azcopy copy '{src_uri}' '{dest_uri}' " \
+                   "--recursive --skip-version-check --output-level essential"
 
-    target_folder_path = os.path.join(output_container, execution_time)
-    # Create the target folder if it doesn't exist
-    os.makedirs(target_folder_path, exist_ok=True)
-
-    # TODO: Refactor this to use the copy_file and delete_file functions.
-    #       copy_file(file_name, input_container, target_folder_path)
-    #       delete_file(os.path.join(input_container, file_name))
-    # Move each file to the target container while preserving the directory structure
-    for source_image_path, relative_image_path in image_paths:
-        target_file_path = os.path.join(target_folder_path, relative_image_path)
-
-        # Create the directory structure in the target folder if it doesn't exist
-        os.makedirs(os.path.dirname(target_file_path), exist_ok=True)
-
-        # Copy the file to the target directory
-        shutil.copy(source_image_path, target_file_path)
-        # TODO do we also want to check the max file size?
-
-        # Verify successful file copy
-        if not os.path.exists(target_file_path):
-            error_message = f"Failed to move file '{relative_image_path}' to the destination: {target_file_path}"
-            logger.error(error_message)
-            raise FileNotFoundError(error_message)
-
-        # Remove the file from the source folder
-        try:
-            os.remove(source_image_path)
-        except OSError:
-            error_message = f"Failed to remove file '{source_image_path}'."
-            logger.error(error_message)
-            raise OSError(error_message)
-
-    logger.info("Files moved and removed successfully.")
+    result = run_cmd(download_cmd)
+    if result:
+        print(f"Failed to download model files with URL: {src_uri}")
+        return False
--- blurring_as_a_service/pre_inference_pipeline/submit_pre_inference_pipeline.py
+++ blurring_as_a_service/pre_inference_pipeline/submit_pre_inference_pipeline.py
@@ -1,25 +1,18 @@
 import os
 from datetime import datetime
 
-from azure.ai.ml import Output
+from azure.ai.ml import Output, Input
 from azure.ai.ml.dsl import pipeline
-
-from blurring_as_a_service.settings.settings import (  # noqa: E402
-    BlurringAsAServiceSettings,
-)
-from blurring_as_a_service.settings.settings_helper import setup_azure_logging
-
-# DO NOT import relative paths before setting up the logger.
-# Exception, of course, is settings to set up the logger.
-BlurringAsAServiceSettings.set_from_yaml("config.yml")
-settings = BlurringAsAServiceSettings.get_settings()
-setup_azure_logging(settings["logging"], __name__)
+from azure.ai.ml.constants import AssetTypes
 
 from blurring_as_a_service.pre_inference_pipeline.components.move_files import (  # noqa: E402
     move_files,
 )
-from blurring_as_a_service.pre_inference_pipeline.components.split_workload import (  # noqa: E402
+from blurring_as_a_service.pre_inference_pipeline.components.split_workload import (
     split_workload,
+)
+from blurring_as_a_service.settings.settings import (  # noqa: E402
+    BlurringAsAServiceSettings,
 )
 from blurring_as_a_service.utils.aml_interface import AMLInterface  # noqa: E402
 
@@ -31,22 +24,11 @@ def pre_inference_pipeline():
         "number_of_batches"
     ]
 
-    move_data = move_files(execution_time=execution_time)
-    azureml_input_formatted = aml_interface.get_datastore_full_path(
-        f"{settings['customer']}_input"
-    )
     azureml_output_formatted = aml_interface.get_datastore_full_path(
         f"{settings['customer']}_input_structured"
     )
 
-    # NOTE We need to use Output to also delete the files.
-    move_data.outputs.input_container = Output(
-        type="uri_folder", mode="rw_mount", path=azureml_input_formatted
-    )
-
-    move_data.outputs.output_container = Output(
-        type="uri_folder", mode="rw_mount", path=azureml_output_formatted
-    )
+    move_data = move_files(execution_time=execution_time)
 
     split_workload_step = split_workload(
         data_folder=move_data.outputs.output_container,
@@ -63,8 +45,12 @@ def pre_inference_pipeline():
 
 
 if __name__ == "__main__":
+    # Retrieve values from the YAML
+    BlurringAsAServiceSettings.set_from_yaml("config.yml")
+    settings = BlurringAsAServiceSettings.get_settings()
     pre_inference_settings = settings["pre_inference_pipeline"]
-    default_compute = settings["aml_experiment_details"]["compute_name"]
+
+    default_compute = pre_inference_settings["compute_name"]
     aml_interface = AMLInterface()
     aml_interface.submit_pipeline_experiment(
         pre_inference_pipeline, "pre_inference_pipeline", default_compute
