0ca557e1d8cc013926982a02612e70c96dfa3131
chrise96 <chrise96@hotmail.com>
2023-11-30 14:22:02 +0100

    Feedback

--- blurring_as_a_service/inference_pipeline/components/detect_and_blur_sensitive_data.py
+++ blurring_as_a_service/inference_pipeline/components/detect_and_blur_sensitive_data.py
@@ -5,7 +5,7 @@ import secrets
 import string
 import sys
 from datetime import datetime
-import shutil
+
 import torch
 import yaml
 from azure.ai.ml.constants import AssetTypes
@@ -107,61 +107,62 @@ def detect_and_blur_sensitive_data(
         if batch_file_txt.endswith('.txt'):
             file_path = os.path.join(batches_files_path, batch_file_txt)
 
-        # Check if the path points to a file (not a directory) and if the file exists
-        if os.path.isfile(file_path) and os.path.exists(file_path):
-            logger.info(f"Creating inference step: {file_path}")
+            # Check if the path points to a file (not a directory) and if the file exists
+            if os.path.isfile(file_path) and os.path.exists(file_path):
+                logger.info(f"Creating inference step: {file_path}")
 
-            files_to_blur_full_path = os.path.join(
-                yolo_yaml_path, batch_file_txt
-            )  # use outputs folder as Azure expects outputs there
+                files_to_blur_full_path = os.path.join(
+                    yolo_yaml_path, batch_file_txt
+                )  # use outputs folder as Azure expects outputs there
 
-            # Define the locked file path with ".lock" extension
-            locked_file_path = file_path + ".lock"
+                # Define the locked file path with ".lock" extension
+                locked_file_path = file_path + ".lock"
 
-            read_success = False
-            try:
-                # Rename the file to add ".lock" extension
-                os.rename(file_path, locked_file_path)
+                read_success = False
+                try:
+                    # Rename the file to add ".lock" extension
+                    os.rename(file_path, locked_file_path)
 
-                with open(locked_file_path, "r") as src:
-                    with open(files_to_blur_full_path, "w") as dest:
-                        for line in src:
-                            dest.write(f"{input_structured_folder}/{line}")
-                read_success = True
-            except FileNotFoundError as e:
-                logger.info(f"File {locked_file_path} not found: {e}")
-            except Exception as e:
-                logger.error(f"Error occurred while reading {locked_file_path}: {e}")
+                    with open(locked_file_path, "r") as src:
+                        with open(files_to_blur_full_path, "w") as dest:
+                            for line in src:
+                                dest.write(f"{input_structured_folder}/{line}")
+                    read_success = True
+                except FileNotFoundError as e:
+                    logger.info(f"File {locked_file_path} not found: {e}")
+                except Exception as e:
+                    logger.error(f"Error occurred while reading {locked_file_path}: {e}")
 
-            data = dict(
-                train=f"{files_to_blur_full_path}",
-                val=f"{files_to_blur_full_path}",
-                test=f"{files_to_blur_full_path}",
-                nc=2,
-                names=["person", "license_plate"],
-            )
+                if read_success:
+                    data = dict(
+                        train=f"{files_to_blur_full_path}",
+                        val=f"{files_to_blur_full_path}",
+                        test=f"{files_to_blur_full_path}",
+                        nc=2,
+                        names=["person", "license_plate"],
+                    )
 
-            # Remove the extension
-            file_name_without_extension = batch_file_txt.rsplit('.', 1)[0]
-            yaml_name = f"{file_name_without_extension}_pano.yaml"
+                    # Remove the extension
+                    file_name_without_extension = batch_file_txt.rsplit('.', 1)[0]
+                    yaml_name = f"{file_name_without_extension}_pano.yaml"
 
-            with open(f"{yolo_yaml_path}/{yaml_name}", "w") as outfile:
-                yaml.dump(data, outfile, default_flow_style=False)
-            cuda_device = torch.cuda.current_device()
-            model_parameters = json.loads(model_parameters_json)
-            database_parameters = json.loads(database_parameters_json)
-            val.run(
-                weights=model,
-                data=f"{yolo_yaml_path}/{yaml_name}",
-                project=results_path,
-                device=cuda_device,
-                name="",
-                customer_name=customer_name,
-                start_time=get_current_time(),
-                run_id=generate_unique_string(10),
-                **model_parameters,
-                **database_parameters,
-            )
-            delete_file(locked_path)
-            delete_file(files_to_blur_full_path)
-            delete_file(f"{yolo_yaml_path}/{yaml_name}")
+                    with open(f"{yolo_yaml_path}/{yaml_name}", "w") as outfile:
+                        yaml.dump(data, outfile, default_flow_style=False)
+                    cuda_device = torch.cuda.current_device()
+                    model_parameters = json.loads(model_parameters_json)
+                    database_parameters = json.loads(database_parameters_json)
+                    val.run(
+                        weights=model,
+                        data=f"{yolo_yaml_path}/{yaml_name}",
+                        project=results_path,
+                        device=cuda_device,
+                        name="",
+                        customer_name=customer_name,
+                        start_time=get_current_time(),
+                        run_id=generate_unique_string(10),
+                        **model_parameters,
+                        **database_parameters,
+                    )
+                    delete_file(locked_file_path)
+                    delete_file(files_to_blur_full_path)
+                    delete_file(f"{yolo_yaml_path}/{yaml_name}")
